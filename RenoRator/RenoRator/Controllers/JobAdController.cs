fe96c8eb4dc4ba4362f3d1e7a04be23449aca4ed
Billy Cai <b.cai.london@gmail.com>
2012-10-05 14:57:23 -0400

    added jobAd controller and view and jquery files

---
+++ RenoRator/RenoRator/Controllers/JobAdController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RenoRator.Models;
+
+namespace RenoRator.Controllers
+{
+    public class JobAdController : Controller
+    {
+        renoRatorDBEntities _db;
+        //
+        // GET: /JobAd/
+
+        public ActionResult Index()
+        {
+            //_db = new renoRatorDBEntities();
+            //var priceRanges = _db.PriceRanges1.ToList();
+            //ViewData["priceranges"] = priceRanges;
+            return View();
+        }
+
+        public ActionResult Post()
+        {
+
+            return View();
+        }
+
+    }
+}
